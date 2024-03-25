e1c0c86b8139145703b90fd731ac296a9a5eb351
Darek Radke <dradmclad@gmail.com>
2024-03-25 13:53:24 -0500

    mobile view formatting for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
   return (
     <main className="lg:max-w-[1200px] m-auto">
       <FadeInSection>
-        <div className="flex flex-col lg:mb-[100px]">
-          <h1 className="font-bold text-5xl lg:mb-[10px]">
+        <div className="flex flex-col lg:mb-[100px] mb-[50px]">
+          <h1 className="font-bold text-5xl lg:mb-[10px] px-2 lg:px-0">
             Get More Business Now
           </h1>
-          <p className="lg:max-w-[850px] lg:pl-[10px] lg:text-2xl">
+          <p className="lg:max-w-[850px] lg:text-2xl px-3 lg:px-0 lg:pl-[10px]">
             Welcome to Venci, where we specialize in empowering small to medium
             businesses with comprehensive marketing solutions tailored to their
             needs. From custom websites to targeted advertising campaigns,
@@ -24,8 +24,8 @@ export default function Home() {
       </FadeInSection>
 
       <FadeInSection>
-        <h1 className="text-5xl lg:mb-[10px] font-bold">Our Work</h1>
-        <div className="flex flex-col lg:flex-row lg:gap-5 lg:h-[165px] lg:mb-[100px]">
+        <h1 className="text-5xl mb-[10px] font-bold px-2 lg:px-0">Our Work</h1>
+        <div className="flex flex-col lg:flex-row lg:gap-5 lg:h-[165px] lg:mb-[100px] mb-[50px]">
           <div className="overflow-hidden w-full lg:w-1/3">
             <Image src={photoImg} alt="photo" />
           </div>
@@ -39,11 +39,11 @@ export default function Home() {
       </FadeInSection>
 
       <FadeInSection>
-      <h1 className="text-5xl mb-[20px] font-bold">Our Services</h1>
+      <h1 className="text-5xl mb-[20px] font-bold px-2 lg:px-0">Our Services</h1>
         <div className="flex flex-col lg:mb-[100px] lg:flex-row gap-5 lg:gap-8">
           <div className="w-full lg:w-1/4 hover:bg-lime-400 transition-colors duration-500">
-            <h1 className="text-5xl lg:ml-1 lg:mb-2">Website Services</h1>
-            <p className="lg:px-2 lg:pb-2">
+            <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">Website Services</h1>
+            <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
               At Venci, we understand the importance of a strong online presence
               for businesses. That's why we offer custom website design as a
               subscription service, providing you with a professional website
@@ -52,8 +52,8 @@ export default function Home() {
             </p>
           </div>
           <div className="w-full lg:w-1/4 hover:bg-pink-400 transition-colors duration-500">
-            <h1 className="text-5xl lg:ml-1 lg:mb-2">Paper Media</h1>
-            <p className="lg:px-2 lg:pb-2">
+            <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">Paper Media</h1>
+            <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
               Elevate your marketing efforts with Venci's paper advertising
               service. Our campaigns include strategically placed QR codes that
               seamlessly integrate with your print materials, driving traffic
@@ -61,8 +61,8 @@ export default function Home() {
             </p>
           </div>
           <div className="w-full lg:w-1/4 hover:bg-amber-300 transition-colors duration-500">
-            <h1 className="text-5xl lg:ml-1 lg:mb-2">Social Media</h1>
-            <p className="lg:px-2 lg:pb-2">
+            <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">Social Media</h1>
+            <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
               Stay ahead of the curve with Venci's social media management
               subscription. Our expert team will handle all aspects of your
               social media presence, ensuring consistent engagement across
@@ -70,8 +70,8 @@ export default function Home() {
             </p>
           </div>
           <div className="w-full lg:w-1/4 hover:bg-cyan-400 transition-colors duration-500">
-            <h1 className="text-5xl lg:ml-1 lg:mb-2">360&deg; Branding</h1>
-            <p className="lg:px-2 lg:pb-2">
+            <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">360&deg; Branding</h1>
+            <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
               At Venci, we understand that branding is more than just a logo -
               it's a reflection of your business identity. Our 360&deg;
               branding services encompass logo development, color scheming, and
