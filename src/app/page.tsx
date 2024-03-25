f44e6f2072f30f26a9dbf038d22b78a5a9a9fb23
Darek Radke <dradmclad@gmail.com>
2024-03-25 14:50:55 -0500

    apostrophe fix for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             Welcome to Venci, where we specialize in empowering small to medium
             businesses with comprehensive marketing solutions tailored to their
             needs. From custom websites to targeted advertising campaigns,
-            social media management, and 360-degree branding services, we're
+            social media management, and 360-degree branding services, we&#39;re
             here to help you elevate your online presence and reach your target
             audience effectively.
           </p>
@@ -45,7 +45,7 @@ export default function Home() {
             <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">Website Services</h1>
             <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
               At Venci, we understand the importance of a strong online presence
-              for businesses. That's why we offer custom website design as a
+              for businesses. That&#39;s why we offer custom website design as a
               subscription service, providing you with a professional website
               that evolves with your business needs without the burden of a
               hefty upfront investment.
@@ -54,7 +54,7 @@ export default function Home() {
           <div className="w-full lg:w-1/4 hover:bg-pink-400 transition-colors duration-500">
             <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">Paper Media</h1>
             <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
-              Elevate your marketing efforts with Venci's paper advertising
+              Elevate your marketing efforts with Venci&#39;s paper advertising
               service. Our campaigns include strategically placed QR codes that
               seamlessly integrate with your print materials, driving traffic
               directly to your website and maximizing your return on investment
@@ -63,7 +63,7 @@ export default function Home() {
           <div className="w-full lg:w-1/4 hover:bg-amber-300 transition-colors duration-500">
             <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">Social Media</h1>
             <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
-              Stay ahead of the curve with Venci's social media management
+              Stay ahead of the curve with Venci&#39;s social media management
               subscription. Our expert team will handle all aspects of your
               social media presence, ensuring consistent engagement across
               platforms and targeted outreach to your desired audience.
@@ -73,7 +73,7 @@ export default function Home() {
             <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">360&deg; Branding</h1>
             <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
               At Venci, we understand that branding is more than just a logo -
-              it's a reflection of your business identity. Our 360&deg;
+              it&#39;s a reflection of your business identity. Our 360&deg;
               branding services encompass logo development, color scheming, and
               comprehensive brand strategy to ensure your brand stands out in a
               crowded market.
