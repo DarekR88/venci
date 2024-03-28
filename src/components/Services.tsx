d3b9ea3a331867c3369d9c7a50a14f52ffbfb954
Darek Radke <dradmclad@gmail.com>
2024-03-28 15:32:19 -0500

    services component finished

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -24,36 +24,44 @@ export function Services() {
           </Link>
         </div>
         <div className="w-full lg:w-1/4 hover:bg-pink-400 transition-colors duration-500">
-          <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">Paper Media</h1>
-          <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
-            Elevate your marketing efforts with Venci&#39;s paper advertising
-            service. Our campaigns include strategically placed QR codes that
-            seamlessly integrate with your print materials, driving traffic
-            directly to your website and maximizing your return on investment
-          </p>
+          <Link href={"/advertising"}>
+            <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">
+              Paper Media
+            </h1>
+            <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
+              Elevate your marketing efforts with Venci&#39;s paper advertising
+              service. Our campaigns include strategically placed QR codes that
+              seamlessly integrate with your print materials, driving traffic
+              directly to your website and maximizing your return on investment
+            </p>
+          </Link>
         </div>
         <div className="w-full lg:w-1/4 hover:bg-amber-300 transition-colors duration-500">
-          <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">
-            Social Media
-          </h1>
-          <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
-            Stay ahead of the curve with Venci&#39;s social media management
-            subscription. Our expert team will handle all aspects of your social
-            media presence, ensuring consistent engagement across platforms and
-            targeted outreach to your desired audience.
-          </p>
+          <Link href={"/social"}>
+            <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">
+              Social Media
+            </h1>
+            <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
+              Stay ahead of the curve with Venci&#39;s social media management
+              subscription. Our expert team will handle all aspects of your
+              social media presence, ensuring consistent engagement across
+              platforms and targeted outreach to your desired audience.
+            </p>
+          </Link>
         </div>
         <div className="w-full lg:w-1/4 hover:bg-cyan-400 transition-colors duration-500">
-          <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">
-            360&deg; Branding
-          </h1>
-          <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
-            At Venci, we understand that branding is more than just a logo -
-            it&#39;s a reflection of your business identity. Our 360&deg;
-            branding services encompass logo development, color scheming, and
-            comprehensive brand strategy to ensure your brand stands out in a
-            crowded market.
-          </p>
+          <Link href={"/advertising"}>
+            <h1 className="text-5xl lg:ml-1 lg:mb-2 px-2 lg:px-0">
+              360&deg; Branding
+            </h1>
+            <p className="lg:px-2 lg:pb-2 px-2 lg:px-0">
+              At Venci, we understand that branding is more than just a logo -
+              it&#39;s a reflection of your business identity. Our 360&deg;
+              branding services encompass logo development, color scheming, and
+              comprehensive brand strategy to ensure your brand stands out in a
+              crowded market.
+            </p>
+          </Link>
         </div>
       </div>
     </main>
